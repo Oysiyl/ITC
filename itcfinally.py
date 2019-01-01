a6f62ae170c7840993ae8913e404cdd0f6c7500b
Dmitriy Kisil <logart1995@gmail.com>
2019-01-01 19:33:51 +0200

    added sources and full text for every article

--- itcfinally.py
+++ itcfinally.py
@@ -26,12 +26,12 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 from tqdm import tqdm
-import time
+import time as t
 
-start0 = time.time()
+start0 = t.time()
 
 #Create list which contains how many pages you want to scraping
-numbers = [*range(1, 26)]
+numbers = [*range(1, 3)]
 #Create an empty list for all of the page adresses
 listadres = []
 #Use for loop to fill list above with page adresses
@@ -63,6 +63,56 @@ def onepage(adres):
     counts = soup.find_all("span", class_ = "comments part")
     #Find preface for topic
     sometext = soup.find_all("div", class_ = "entry-excerpt hidden-xs")
+    #Find full text for topic
+    fulltext = soup.find_all("div", class_ = "col-xs-8 col-txt")
+    #Create an empty lists
+    listlinks = fulltext
+    listfulltext = []
+    listsources = []
+    #Fill the lists above our scraping date
+    # print(listlinks)
+    #Fill the lists above our scraping date
+    for i in range(0, 24):
+        k = (
+            str(listlinks[i])
+            .split('" rel="bookmark">')[0]
+            .replace('<div class="col-xs-8 col-txt">','')
+            .replace('<h2 class="entry-title text-uppercase bold_title">','')
+            .replace('<h2 class="entry-title text-uppercase "> ', '')
+            .replace("[' ', ","")
+            .replace('<a href="','')
+            .replace("[","")
+            )
+        page2 = requests.get(k, headers = headers, timeout=5)
+        soup2 = BeautifulSoup(page2.content, 'html.parser')
+        d = soup2.find("div", class_ = "post-txt")
+        if d is None:
+            d = soup2.find("div", class_ = "container-fluid txt")
+        elif len(list(set(list(d.get_text())))) == 1:
+            d = soup2.find_all("div", class_ = "post-txt")
+            d = d[1]
+        d = d.get_text()
+        d = " ".join(d.split())
+        d = (
+            str(d)
+            .split('.articles-meta')[0]
+            .split('.post-facts')[0]
+            .replace(".", " . ")
+            )
+        listfulltext.append(d)
+        k = (
+            d
+            .replace("Источник:","_I_")
+            .replace("Источники:","_I_")
+            .split("_I_")[-1]
+            .replace(" . ", ".")
+            .replace(" .", ".")
+            )
+        if k is None:
+            listsources.append("")
+        else:
+            listsources.append(k)
+        t.sleep(1.5)
     #Create an empty lists
     listtitle = []    
     listtime = []
@@ -91,7 +141,9 @@ def onepage(adres):
             "time": listtimeup,
             "author": listauthor,            
             "counts": listcounts,
-            "sometext": listsometext})
+            "sometext": listsometext,
+            "fulltext": listfulltext,
+            "listsources": listsources})
     #Function will return that DataFrame    
     return df
     
@@ -103,12 +155,12 @@ for c,v in tqdm(enumerate(listadres)):
     u = 100/(len(listadres))
     percent = int(round(u*c,2))
     print("Currently done " + str(percent) + "%")
-    time.sleep(1.5)
+
 #Check df
 print(df)
 #Save DataFrame to csv
 df.to_csv("itctray.csv")
-end0 = time.time()
+end0 = t.time()
 elapsed_time0 = end0 - start0
-elapsed_time0 = time.strftime("%H:%M:%S", time.gmtime(elapsed_time0))
+elapsed_time0 = t.strftime("%H:%M:%S", t.gmtime(elapsed_time0))
 print("Mission complete in " + str(elapsed_time0))
