32abfae8e6d53d6d0801d13b28a74ed4adccd144
Dmitriy Kisil <logart1995@gmail.com>
2019-02-07 15:40:04 +0200

    flake8

--- itcfinally.py
+++ itcfinally.py
@@ -1,68 +1,175 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 17 18:10:03 2018
+Created on Thu Jul 12 16:44:53 2018.
 
 @author: dmitriy
 """
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul 16 10:28:26 2018
-
-@author: dmitriy
-"""
-
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 12 16:44:53 2018
-
-@author: dmitriy
-"""
-#import all the necessary libraries
-import pandas as pd
+# import all the necessary libraries
 from bs4 import BeautifulSoup
+import datetime as dt
+import pandas as pd
 import requests
-from tqdm import tqdm
 import time as t
+from tqdm import tqdm
 
 start0 = t.time()
 
-#Create list which contains how many pages you want to scraping
-numbers = [*range(1, 3)]
-#Create an empty list for all of the page adresses
-listadres = []
-#Use for loop to fill list above with page adresses
-for i in numbers:
-    if i == 1:
-        example = "https://itc.ua/"
+
+def catch_error(something):
+    """Try catch an exception.
+
+    __Attributes__
+        something: Where we will search for Exception.
+
+    __Returns__
+        something if not error or "" if error is.
+
+    """
+    try:
+        something
+    except IndexError:
+        something = ""
+        print("An Index Error!")
+        return something
     else:
-        example = "https://itc.ua/page/" + str(i) + "/"
-    listadres.append(example)
-#Check output    
-print(listadres)
-#Create function that scraping full one page from site
+        # print(something)
+        return something
+
+
+list2 = ['18-03-2012', '21-03-2012']  # d-m-y
+list1 = [1, 2]
+
+
+def get_search_query(list2, t):
+    """Create a query, that we will parse.
+
+    __Attributes__
+        list2: List, which contain start and end date.
+        t: if 1 change search query (look below)
+
+    __Returns__
+        search_query: List, which contain two search queries.
+
+    """
+    if t > 1:
+        search_query = ("https://itc.ua/page/" + str(t)
+                        + "/?s&after=" + str(list2[0])
+                        + "&before=" + str(list2[1])
+                        )
+    else:
+        search_query = ("https://itc.ua/?s&after=" + str(list2[0])
+                        + "&before=" + str(list2[1])
+                        )
+    return search_query
+
+
+def prep_numbers_2(search_query, list2, list1):
+    """Prep list to parse.
+
+    __Attributes__
+        search_query: list with dates.
+        list1: list with dates.
+        list2: list with numbers of pages to parse.
+
+    __Returns__
+        listadres: list with data to parse.
+
+    """
+    # https://itc.ua/?s&after=10-01-2019&before=12-01-2019
+    # https://itc.ua/page/3/?s&after=10-01-2019&before=12-01-2019
+    # Create list which contains how many pages you want to scraping
+    numbers = list(range(list1[0], list1[1]+1))
+    # Create an empty list for all of the page adresses
+    listadres = []
+    # Use for loop to fill list above with page adresses
+    for i in numbers:
+        if i == 1:
+            example = ("https://itc.ua/?s&after=" + str(list2[0])
+                       + "&before=" + str(list2[1]))
+        else:
+            example = ("https://itc.ua/page/" + str(i)
+                       + "/?s&after=" + str(list2[0])
+                       + "&before=" + str(list2[1])
+                       )
+        listadres.append(example)
+    # Check output
+    print(listadres)
+    return listadres
+
+
+search_query = get_search_query(list2, 0)
+listadres = prep_numbers_2(search_query, list2, list1)
+
+
+def prep_numbers(list1):
+    """Prep list to parse.
+
+    __Attributes__
+        list1: list with numbers of pages to parse.
+
+    __Returns__
+        listadres: list with data to parse.
+
+    """
+    # https://itc.ua/?s&after=10-01-2019&before=12-01-2019
+    # https://itc.ua/page/3/?s&after=10-01-2019&before=12-01-2019
+    # Create list which contains how many pages you want to scraping
+    numbers = list(range(list1[0], list1[1]+1))
+    # Create an empty list for all of the page adresses
+    listadres = []
+    # Use for loop to fill list above with page adresses
+    for i in numbers:
+        if i == 1:
+            example = "https://itc.ua/"
+        else:
+            example = "https://itc.ua/page/" + str(i) + "/"
+        listadres.append(example)
+    # Check output
+    print(listadres)
+    return listadres
+
+# listadres = prep_numbers(list1)
+# Create function that scraping full one page from site
+
+
 def onepage(adres):
-    #Use headers to prevent hide our script
+    """Take one query and get DataFrame.
+
+    __Attributes__
+        adres: one query.
+
+    __Returns__
+        df: DataFrame with parsed values.
+
+    """
+    # Use headers to prevent hide our script
     headers = {'User-Agent': 'Mozilla/5.0'}
-    #Get page
-    page = requests.get(adres, headers = headers, timeout=5)
-    #Get all of the html code 
+    # Get page
+    page = requests.get(adres, headers=headers)  # read_timeout=5
+    # Get all of the html code
     soup = BeautifulSoup(page.content, 'html.parser')
-    #Find title the topic
-    title = soup.find_all("h2", class_ = "entry-title")
-    #Find time when topic is published 
-    time = soup.find_all("time", class_ = "published")
-    #Find time when topic is updated
-    timeup = soup.find_all("time", class_ = "screen-reader-text updated")
-    #Find author the topic
-    author = soup.find_all("a", class_ = "screen-reader-text fn")
-    #Find how many comments have topic
-    counts = soup.find_all("span", class_ = "comments part")
-    #Find preface for topic
-    sometext = soup.find_all("div", class_ = "entry-excerpt hidden-xs")
+    header = soup.find("header", class_="entry-header")
+    # header_text = header.get_text()
+    if header is not None:
+        print("That's all!")
+        return None
+
+    # Find title the topic
+    title = soup.find_all("h2", class_="entry-title")
+    # Find time when topic is published
+    time = soup.find_all("time", class_="published")
+    # Find time when topic is updated
+    timeup = soup.find_all("time", class_="screen-reader-text updated")
+    # Find author the topic
+    author = soup.find_all("a", class_="screen-reader-text fn")
+    # Find how many comments have topic
+    counts = soup.find_all("span", class_="comments part")
+    # Find preface for topic
+    sometext = soup.find_all("div", class_="entry-excerpt hidden-xs")
+    # Category
+    category = soup.find_all("span", class_="cat part text-uppercase")
+    '''
     #Find full text for topic
     fulltext = soup.find_all("div", class_ = "col-xs-8 col-txt")
     #Create an empty lists
@@ -83,7 +190,7 @@ def onepage(adres):
             .replace('<a href="','')
             .replace("[","")
             )
-        page2 = requests.get(k, headers = headers, timeout=5)
+        page2 = requests.get(k, headers = headers)
         soup2 = BeautifulSoup(page2.content, 'html.parser')
         d = soup2.find("div", class_ = "post-txt")
         if d is None:
@@ -91,8 +198,9 @@ def onepage(adres):
         elif len(list(set(list(d.get_text())))) == 1:
             d = soup2.find_all("div", class_ = "post-txt")
             d = d[1]
-        d = d.get_text()
-        d = " ".join(d.split())
+        if d is not None:
+            d = d.get_text()
+            d = " ".join(d.split())
         d = (
             str(d)
             .split('.articles-meta')[0]
@@ -113,53 +221,185 @@ def onepage(adres):
         else:
             listsources.append(k)
         t.sleep(1.5)
-    #Create an empty lists
-    listtitle = []    
+    '''
+    # Create an empty lists
+    listtitle = []
     listtime = []
     listtimeup = []
     listauthor = []
     listcounts = []
     listsometext = []
-    #Fill the lists above our scraping date
-    for i in range(0, 24):
-        k = title[i].get_text()
+    listcategory = []
+
+    limit = min([len(list(title)),
+                 len(list(time)),
+                 len(list(timeup)),
+                 len(list(timeup)),
+                 len(list(author)),
+                 len(list(counts)),
+                 len(list(sometext)),
+                 len(list(category))
+                 ]
+                )
+
+    # Fill the lists above our scraping date
+    for i in range(0, limit):
+        k = title[i].get_text().replace("\n", "").replace("\t", "")
         listtitle.append(k)
-        l = time[i].get_text()
-        listtime.append(l)
-        m = timeup[i].get_text()
+        # listtitle = " ".join(title[i].get_text().split())
+        ll = time[i].get_text().replace("\n", "").replace("\t", "")
+        listtime.append(ll)
+        m = timeup[i].get_text().replace("\n", "").replace("\t", "")
         listtimeup.append(m)
-        n = author[i].get_text()
+        try:
+            n = author[i].get_text().replace("\n", "").replace("\t", "")
+        except IndexError:
+            n = ""
         listauthor.append(n)
-        o = counts[i].get_text()
+        # n = catch_error(author[i].get_text())
+        # listauthor.append(n)
+        o = counts[i].get_text().replace("\n", "").replace("\t", "")
         listcounts.append(o)
-        p = sometext[i].get_text()
+        try:
+            p = sometext[i].get_text().replace("\n", "").replace("\t", "")
+        except IndexError:
+            p = ""
         listsometext.append(p)
-    #Create DataFrame, that will contains info from lists
+        c = category[i].get_text()
+        listcategory.append(c)
+
+    # Create DataFrame, that will contains info from lists
     df = pd.DataFrame({
             "title": listtitle,
             "date": listtime,
             "time": listtimeup,
-            "author": listauthor,            
+            "author": listauthor,
             "counts": listcounts,
             "sometext": listsometext,
-            "fulltext": listfulltext,
-            "listsources": listsources})
-    #Function will return that DataFrame    
+            "category": listcategory,
+            # "fulltext": listfulltext,
+            # "listsources": listsources
+            })
+    # Function will return that DataFrame
     return df
-    
-#Create an empty DataFrame
-df = pd.DataFrame()
-#Adding each new page in one DataFrame
-for c,v in tqdm(enumerate(listadres)):
-    df = pd.concat([df, onepage(v)], ignore_index = True)
-    u = 100/(len(listadres))
-    percent = int(round(u*c,2))
-    print("Currently done " + str(percent) + "%")
 
-#Check df
-print(df)
-#Save DataFrame to csv
-df.to_csv("itctray.csv")
+
+# Create an empty DataFrame
+df = pd.DataFrame()
+
+# Adding each new page in one DataFrame
+for c, v in tqdm(enumerate(listadres)):
+    if onepage(v) is None:
+        break
+    else:
+        t.sleep(1.5)
+    df = pd.concat([df, onepage(v)], ignore_index=True)
+    u = 100/(len(listadres))
+    percent = int(round(u*(c+1), 2))
+    print("Currently done " + str(percent) + "%")
+    listadres.append(get_search_query(list2, c+1))
+    t.sleep(1.5)
+# print(df.date)
+df['date'] = df['date'].str.replace(" в ", "/")
+# print(df.date)
+
+# df['date']
+'''
+df0 = pd.read_csv("itctray.csv")
+df0 = df0.loc[:, ~df0.columns.str.contains('^Unnamed')]
+# del df["date"]
+print(df0.head())
+#df0 = df0[df.columns]
+# df0 = df0.sort_values("Date")
+# df0 = df0.reset_index(drop=True)
+# df0.to_csv("itctray.csv")
+print(df0.dtypes)
+df = pd.concat([df0,df],ignore_index=True, sort=True)
+'''
+df['Date'] = pd.to_datetime(df['date'], format="%d.%m.%Y/%H:%M")
+
+
+def get_years(df):
+    """Save parsed data from DataFrame by year in each csv file.
+
+    __Attributes__
+        search_query: list with dates.
+        list1: list with dates.
+        list2: list with numbers of pages to parse.
+
+    __Returns__
+        listadres: list with data to parse.
+
+    """
+    list1 = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018]
+    for i in list1:
+        df = df[df['Date'].dt.year == i]
+        # 3:19 PM 13/12/2018
+        # Change datetime format to what we want (example above)
+
+        df['Date'] = (
+                df['Date']
+                .apply(lambda x: dt.datetime.strftime(x, '%I:%M %p %d/%m/%Y'))
+                )
+
+        print(df['Date'])
+
+        df.drop_duplicates(subset='title', inplace=True)
+        print(df.dtypes)
+        df = df.sort_values("date", ascending=False)
+        # df = df.sort_values("Date")
+        df = df.reset_index(drop=True)
+        print(df['Date'])
+
+        # Check df
+        # df = df[["title","date","Date","time","author","counts","sometext",
+        #           "fulltext","listsources"]]
+        print(df)
+        # Save DataFrame to csv
+        # df.to_csv("itctray.csv")
+        df.to_csv("year_" + str(i) + ".csv")
+        # df.to_csv("oneyear2018.csv")
+# get_years(df)
+
+
+def get_one_csv(df):
+    """Prepare DataFrame.
+
+    __Attributes__
+        df: DataFrame, which need to be prepared.
+
+    __Returns__
+        df: DataFrame, that prepared.
+
+    """
+    # 3:19 PM 13/12/2018
+    # Change datetime format to what we want (example above)
+
+    df['Date'] = (
+            df['Date']
+            .apply(lambda x: dt.datetime.strftime(x, '%I:%M %p %d/%m/%Y'))
+            )
+
+    # print(df['Date'])
+
+    df.drop_duplicates(subset='title', inplace=True)
+    # print(df.dtypes)
+    df = df.sort_values("date", ascending=False)
+    # df = df.sort_values("Date")
+    df = df.reset_index(drop=True)
+    # print(df['Date'])
+
+    # Check df
+    # df = df[["title","date","Date","time","author","counts","sometext",
+    #          "fulltext","listsources"]]
+    print(df)
+    # Save DataFrame to csv
+    df.to_csv("itctray.csv")
+    # df.to_csv("all_years.csv")
+    # df.to_csv("oneyear2018.csv")
+
+
+get_one_csv(df)
 end0 = t.time()
 elapsed_time0 = end0 - start0
 elapsed_time0 = t.strftime("%H:%M:%S", t.gmtime(elapsed_time0))
